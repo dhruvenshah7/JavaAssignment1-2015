8186da945dd6fa2e5f90af19141f60ac1c5d22e5
c0662834 <c0662834@SB-N208-08.lambton.on.ca>
2015-09-14 16:19:05 -0400

    Initial commit

---
+++ src/javaasssignment1/pkg2015/JavaAsssignment12015.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaasssignment1.pkg2015;
+
+/**
+ *
+ * @author c0662834
+ */
+public class JavaAsssignment12015 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello World !!!");
+        
+        /*
+        
+1. 	Java Architect
+	Location: Waterloo, ON
+	http://www.indeed.ca/cmp/Cisco---Webex/jobs/Java-Architect-43ad8c77551d82a4?sjdu=vQIlM60yK_PwYat7ToXhk1vpuZ7FmGYZUPtxDZ6DRDEJTiXacLgxaV6Arc6YEPCoWG73cvQSXBMMiQDRAoMfbw
+
+
+
+2.	J2EE - Java Platform, Enterprise Edition - https://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition
+
+	SPRING -  application framework for the Java platform - https://en.wikipedia.org/wiki/Spring_Framework
+
+	JPA - Java Persistence API - https://en.wikipedia.org/wiki/Java_Persistence_API
+
+	JAVA SE - Java Platform, Standard Edition - https://en.wikipedia.org/wiki/Java_Platform,_Standard_Edition
+
+	JUnit - unit testing framework for the Java - https://en.wikipedia.org/wiki/JUnit
+
+	JQuery -  cross-platform JavaScript library for client side html scripting - https://en.wikipedia.org/wiki/JQuery
+
+	Bootstrap - HTML, CSS, and JS framework for developing front-end of web application - http://getbootstrap.com/
+
+	HTML - HyperText Markup Language - https://en.wikipedia.org/wiki/HTML
+
+	CSS - Cascading Style Sheets - https://en.wikipedia.org/wiki/Cascading_Style_Sheets
+
+	JavaScript -  high level, dynamic, untyped, and interpreted programming language - https://en.wikipedia.org/wiki/JavaScript
+
+	UML - Unified Modeling Language - https://en.wikipedia.org/wiki/Unified_Modeling_Language
+
+	JSON - JavaScript Object Notation - https://en.wikipedia.org/wiki/JSON
+
+	XML - Extensible Markup Language - https://en.wikipedia.org/wiki/XML
+
+	Tomcat - an open-source web server and servlet container - https://en.wikipedia.org/wiki/Apache_Tomcat
+
+	PL/SQL - Procedural Language/Structured Query Language
+
+
+
+3. 	Toronto Salary - $89k
+	Vancouver Salary - $71k
+	Waterloo Salary - $87k
+        
+        */
+    }
+    
+}
